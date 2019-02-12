e29dcbf700d5e8d2b58474c4890ae69d3813f641
ayyleesh <xxserpentxbizzarexx@yahoo.co.id>
2019-02-12 01:27:28 +0000

    modified interface

--- src/script.js
+++ src/script.js
@@ -1,30 +1,45 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const today = document.querySelector('.day');
+const todaysDate = document.querySelector('.date');
 const digitalClock = document.querySelector('.digital');
 
 function setTime() {
   const now = new Date();
 
-  const seconds = now.getUTCSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  var seconds = now.getUTCSeconds();
+  var mins = now.getUTCMinutes();
+  var hours = now.getUTCHours();
+  var date = now.getDate();
+  var month = now.getMonth();
 
-  const mins = now.getUTCMinutes();
-  const minsDegrees = ((mins / 60) * 360) + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  var days = ['Sun','Mon','Tue','Wed','Thur','Fri','Sat'];
 
-  const hours = now.getUTCHours();
-  const hourDegrees = ((hours / 12) * 360) + ((mins / 60) * 30) + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  seconds = (seconds < 10) ? `0${seconds}` : seconds;
+  mins = (mins < 10) ? `0${mins}` : mins;
+  hours = (hours < 10) ? `0${hours}` : hours;
+  date = (date < 10) ? `0${date}` : date;
+  month = (date < 10) ? `0${month + 1}` : month + 1;
 
-  if (seconds < 10) {
-      digitalClock.innerHTML = `${hours}:${mins}:0${seconds}`;
-  } else{
-    digitalClock.innerHTML = `${hours}:${mins}:${seconds}`;
+  digitalClock.innerHTML = `${hours} : ${mins} : ${seconds}`;
+  todaysDate.innerHTML = `${days[now.getDay()]}, ${date}-${month}-${now.getFullYear()}`;
+
+  secondHand.style.setProperty('transform', `rotate(${((seconds / 60) * 360) + 90}deg)`);
+  minsHand.style.setProperty('transform', `rotate(${((mins / 60) * 360) + 90}deg)`);
+  hourHand.style.setProperty('transform', `rotate(${((hours / 12) * 360) + 90}deg)`);
+
+  if(seconds==0){
+    secondHand.style.transitionDuration = '0s';
+    minuteHand.style.transitionDuration = '0s';
+    hourHand.style.transitionDuration = '0s';
+  } else {
+    secondHand.style.transitionDuration = '0.05s';
+    minuteHand.style.transitionDuration = '0.05s';
+    hourHand.style.transitionDuration = '0.05s';﻿
   }
-}
 
+}
 
 setInterval(setTime, 1000);
 setTime();
