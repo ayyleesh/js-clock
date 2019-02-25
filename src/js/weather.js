39b416f169a809a05dc35a88e9277a1707281c07
ayyleesh <xxserpentxbizzarexx@yahoo.co.id>
2019-02-25 18:56:11 +0000

    changed api to openweathermap

--- src/js/weather.js
+++ src/js/weather.js
@@ -9,28 +9,19 @@ window.addEventListener("load", () => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const proxy = "http://cors-anywhere.herokuapp.com/"
-      const api = `${proxy}https://api.darksky.net/forecast/037712230405346e2232e500f0022fdb/${lat},${long}`;
+      const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=0cbe3f02f790bc9891bb444de79f81d6`;
 
       fetch(api)
         .then(response => {
           return response.json();
         })
         .then(data =>{
-          console.log(data);
-          celcius = 5/9*(data.currently.temperature - 32);
+          celcius = data.main.temp-273;
           temp.innerHTML = `${celcius.toFixed(1)}°C`;
-          icon = data.currently.icon;
-          setIcons(icon, document.querySelector('.icon'));
+          icon = data.weather[0].id;
+          document.querySelector('.icon').innerHTML = `<i class="owf owf-${icon} owf-2x"></i>`;
         })
         document.querySelector('.no-location').style.display = 'none';
     });
-  }
-  function setIcons(icon, iconID) {
-    const skycons = new Skycons({color: "#00f2c3"});
-    const currentIcons = icon.replace(/-/g, "_").toUpperCase();
-    skycons.pause();
-    return skycons.set(iconID, Skycons[currentIcons]);
-    console.log(currentIcons);
   }
 });
